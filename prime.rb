ccf61bf6a1c96b52d43e47c85db81c115db223f7
Eric Loudon <loudon.eric@gmail.com>
2019-08-15 11:24:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 # Add  code here!
 def prime?(number)
   if number >= 2
+    # if numbers are greater than or equal to 2
     (2..number - 1).all? do |n|
+      # create range from 2 to highest number(s) -1 = n
       number % n != 0
+      # divide numbers by n !reverse logical = 0
     end
   else
     return false
+
   end
 end
